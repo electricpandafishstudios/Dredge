de10b4fa1dec49b98e86313fc3ef936eb5378159
sbrow <foxtrotalphalima@gmail.com>
2014-05-05 13:54:55 -0400

    foundation code

---
+++ init.lua
@@ -0,0 +1,12 @@
+name = "Dredge"
+long_name = "A Coffeebreak roguelike about decay."
+short_name = "dredge"
+author = { "ElectricPandaFish", "electricpandafishstudios@googlegroups.com" }
+homepage = "http://te4.org/modules:example"
+version = {1,1,5}
+engine = {1,1,5,"te4"}
+description = [[
+...
+]]
+starter = "mod.load"
+show_only_on_cheat = false -- Example modules are not shown to normal players
